cda61fc53bc0337b4413b1c9eb623ef322a73e82
MFvanRossum <matissev@hotmail.com>
2020-01-27 14:50:08 -0500

    problem 10 correct

--- homework.js
+++ homework.js
@@ -134,3 +134,22 @@ function multigreeting(name, lang) {
 
 // Write a function called gcd that takes two arguments and returns the greatest common
 // divisor using the instructions above.
+
+function gcd(a,b) {
+    var d=0
+    while (a % 2 === 0 && b % 2 ===0) {
+        a = a / 2
+        b = b / 2
+        var d = d + 1 
+    }
+    while (a != b) {
+        if (a % 2 === 0) {
+            a = a / 2
+        } else if (b % 2 === 0) {
+            b = b / 2
+        } else if (a > b) {
+            a = ((a - b) / 2)
+        } else b = ((b - a) / 2)
+    } var g = a
+    return g * 2**d
+}
